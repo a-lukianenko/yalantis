17a87a5a8e04850661421b41e298a2b651241e6e
Andrii Lukianenko <lukyanenko.andrew@gmail.com>
2021-05-29 15:41:29 +0300

    feat: add hg-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Employees } from './pages/Employees';
 import { Switch, Redirect, Route } from 'react-router-dom';
-import { NotFound } from 'pages/NotFound';
+import { NotFound } from './pages/NotFound';
 import './App.css';
 
 function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from 'app/store';
+import store from './app/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={'/yalantis/'}>
         <App />
       </Router>
     </Provider>
