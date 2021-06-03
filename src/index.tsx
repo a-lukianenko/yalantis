c82e812984ba03496b308a60c61993a18da3632e
Andrii Lukianenko <lukyanenko.andrew@gmail.com>
2021-06-03 12:29:10 +0300

    fix: switch to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './app/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
